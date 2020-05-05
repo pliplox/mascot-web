671b21a1080cdca58dd2775278af3973e5a3272a
Matias Barrera <matias.barrera95@gmail.com>
2020-05-04 22:04:15 -0400

    chore: test deploy heroku

--- server.js
+++ server.js
@@ -7,6 +7,4 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build','index.html'));
 });
-
-console.log('hola');
 app.listen(3000);

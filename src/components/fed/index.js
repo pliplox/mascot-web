84ba34e32a258be4f6f01e631f9192e130d60c8f
Matias Barrera <matias.barrera95@gmail.com>
2020-06-20 12:04:07 -0400

    chore: blank line

--- src/components/fed/index.js
+++ src/components/fed/index.js
@@ -1,5 +1,5 @@
 //export { default as Navbar } from "./Navbar";
-export {default as EditButton} from './EditButton'
-export {default as EditTextField} from './EditTextField'
-export {default as Fed} from './Fed'
-export {default as TableFed} from './TableFed'
+export { default as EditButton } from "./EditButton";
+export { default as EditTextField } from "./EditTextField";
+export { default as Fed } from "./Fed";
+export { default as TableFed } from "./TableFed";
